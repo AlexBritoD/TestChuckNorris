b8b5d15d3a97c4bff57c27fafb0759b6a4ba4421
Alejandro Brito <alejandro.britod@elektra.com.mx>
2023-06-13 22:24:00 -0600

    Se optimiza el codigo para que tarde por mucho 7 segundos

--- chuck.py
+++ chuck.py
@@ -1,11 +1,42 @@
 from flask import jsonify
 import requests
+import concurrent.futures
+
 
 def handle():
-    url='https://api.chucknorris.io/jokes/random'
     id = set()
-    while len(id) < 25:
-        response = requests.get(url)
-        id.add(response.json()['id'])
+    times = 25
+    results = get_data(times)
+    id.update(results)
+    
+    for x in id:
+        count = list(id).count(x)
+        if count < 1:
+            id.remove(x)   
+            
+    long = len(id)
+    
+    id = set_data(id, long)    
+
     return jsonify(list(id))
 
+def fetch_joke(url):
+    response = requests.get(url)
+    return response.json()['id']
+
+def get_data(times):
+    url = 'https://api.chucknorris.io/jokes/random'
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(fetch_joke, url) for _ in range(times)]
+        results = [future.result() for future in concurrent.futures.as_completed(futures)]
+        return results
+    
+def set_data(id, long):
+    
+    while len(id) < 25:
+        times = 25 - long
+        results = get_data(times)
+        for x in results:
+            id.add(x)
+    
+    return id
